eafa65cadc19d671774e33021af4401ea7cc6ca2
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-26 09:02:02 +0700

    write patch function to transactionitem

--- routes/transactionItem/editItem.js
+++ routes/transactionItem/editItem.js
@@ -1,5 +1,25 @@
 const express = require('express')
-// const addData = require('../../controllers/addController')
+const editData = require('../../controllers/editController')
 const app = express.Router()
+
+app.patch('/transaction/item', (req, res) => {
+    if (Object.keys(req.body).length == 0) return res.status(400).send('body not allowed')
+    if (!req.query.id) return res.status(400).send('id in query needed')
+    if (req.body.id) return res.status(400).send('id in body not allowed')
+    if (!req.body.itemId) {
+        return res.status(400).send('item id in body needed')
+    }
+    else {
+        const { transactionId, itemId, quantity } = req.body
+        const transactionItem = {
+            transactionId,
+            itemId,
+            quantity,
+            "price": getData('items', { id: itemId }).itemPrice * quantity
+        }
+        const result = editData('transactionItem', req.query.id, transactionItem)
+        res.status(200).send(result)
+    }
+})
 
 module.exports = app
