f47eda6f5df977688412131548d72707ce4286ab
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-26 10:26:23 +0700

    updating add method in transaction

--- routes/transaction/add.js
+++ routes/transaction/add.js
@@ -3,25 +3,31 @@ const app = express.Router()
 const uid = require('uid')
 const addData = require('../../controllers/addController')
 const getData = require('../../controllers/getController')
+const dateToday = require('../../helpers/dateToday')
 
 //** add transactions */
 app.post('/transaction', (req, res) => {
     if (Object.keys(req.query).length > 0) return res.status(400).send('query not allowed')
-    if (Object.keys(req.body).length == 0) return res.status(400).send('body not allowed')
-    const id = uid()
-    req.body.id = id
-    const isId = getData('transaction', { id })
-    console.log(isId);
-    if (isId.length == 0) {
-        const isAdded = addData('transaction', req.body)
-        if (isAdded) {
-            res.status(200).send('ok')
-        }
-        else {
-            res.status(400).send('not allowed')
-        }
+    if (String(Object.keys(req.body)) != "userId") return res.status(400).send('just define userId to body')
+    const bodyStructure = {
+        "id": uid(),
+        "userId": req.body.userId,
+        "dateTransaction": dateToday(),
+        "totalProduct": 0,
+        "totalPrice": 0,
+        "discountId": "",
+        "amountTransaction": 0,
+        "payment": 0,
+        "paymentStatus": false
     }
-    res.status(400).send('not allowed')
+    const result = addData('transaction', bodyStructure)
+    if (result) {
+        res.status(200).send(result)
+    }
+    else {
+        res.status(400).send('something bad happend')
+    }
+
 })
 
 module.exports = app
