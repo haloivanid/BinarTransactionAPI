fd06546b8a4865bc44755394601cb5fcd2b6aeab
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-26 08:41:44 +0700

    write at add transactionItem

--- routes/transactionItem/addItem.js
+++ routes/transactionItem/addItem.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const addData = require('../../controllers/addController')
 const uid = require('uid')
+const dateChecker = require('../../helpers/dateChecker')
 const app = express.Router()
 
-app.post('/item', (req, res) => {
+app.post('/transaction/item', (req, res) => {
   if (Object.keys(req.query).length > 0) return res.status(400).send('query not allowed')
   if (Object.keys(req.body).length == 0) return res.status(400).send('body not allowed')
   if (req.body.id) return res.status(400).send('id in body not allowed')
-  //** reserved for add transaction item */
+  if (!req.body.itemId) {
+    return res.status(400).send('item id in body needed')
+  }
+  else {
+    const { transactionId, itemId, quantity } = req.body
+    const transactionItem = {
+      "id": uid(),
+      transactionId,
+      itemId,
+      quantity,
+      "price": getData('items', { id: itemId }).itemPrice * quantity
+    }
+    const result = addData('transactionItem', transactionItem)
+    res.status(200).send(result)
+  }
 })
 
 module.exports = app
