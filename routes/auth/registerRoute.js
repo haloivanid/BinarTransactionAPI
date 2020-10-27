32e52294028c4ae0d2d9fbd0a5058b357302423e
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-27 10:17:35 +0700

    add role to user

--- routes/auth/registerRoute.js
+++ routes/auth/registerRoute.js
@@ -9,6 +9,7 @@ const uid = require('uid')
 
 app.post('/auth/register', (req, res) => {
   const body = req.body
+  body.role = 'customer'
   // untuk mengecek value username dan password agar tidak kosong
   if (body.password == "" || body.username == "") { return res.status(400).send('please input username and password') }
   // mengecek apakah user memasukkan kata administrator sebagai username. 
