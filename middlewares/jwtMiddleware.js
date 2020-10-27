e5f7deebefba6e7840ae2789fec2e5d1e7762851
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-27 22:23:29 +0700

    clear jwt unused function

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -16,19 +16,6 @@ function signJwt(data) {
  * @param {String} token jwt token
  * @returns {any} returns data payload
  */
-
-const test = (req, res, next) => {
-  const authorization = req.headers.authorization
-  const secretKey = jwtConfig.secret
-  const token = authorization.split(' ')[1]
-  if (!token) return res.status(400).json("Token is not found!")
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) return res.status(403).json("Token decode failure!")
-    req.userData = decoded;
-    next()
-  })
-}
-
 function verifyJwt(credential) {
   return (req, res, next) => {
     const authorization = req.headers.authorization
