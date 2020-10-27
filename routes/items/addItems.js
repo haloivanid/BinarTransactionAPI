8a350cb3ae131a11690a35754b37548e007e4963
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-27 10:54:00 +0700

    solve additem bug

--- routes/items/addItems.js
+++ routes/items/addItems.js
@@ -13,10 +13,10 @@ const uid = require('uid')
 
 app.post('/items', auth.verifyJwt(['admin']), (req, res) => {
     const body = req.body
+    body.itemPrice = parseInt(body.itemPrice)
     const isItemExist = getData('items', body)
     if (!isItemExist.length) {
         body.id = uid()
-        body.itemPrice = parseInt(body.itemPrice)
         const result = addData('items', body)
         if (result) {
             res.send(result)
