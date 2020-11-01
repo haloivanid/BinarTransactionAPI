bf900e6059fc018982bdfadf233b24d1d9785563
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-11-02 06:49:33 +0700

    solve problem in customer and admin edit

--- routes/user/admin/editAdmin.js
+++ routes/user/admin/editAdmin.js
@@ -7,19 +7,22 @@ const app = express.Router()
 
 app.patch('/admin', auth.verifyJwt(['admin']), (req, res) => {
   const body = req.body
-  const isUserExists = getData('admin', { username: req.body.username })
-  if (isUserExists.length) {
-    const id = isUserExists[0].id
-    const result = editData('admin', id, body)
-    if (result) {
-      return res.send(result)
-    } else {
-      // called if request body object key is lacking
-      res.status(400).send('Bad request')
-    }
+  if (Object.keys(body).length == 0) return res.status(400).send('body not allowed')
+  //** mengecek apakah user memasukkan string kosong ke dalam body */
+  for (const keys in body) {
+    if (body[keys] == "") return res.status(400).send("value of keys in body can't be empty")
+  }
+  if (!body.password) return res.status(400).status('password needed')
+  const userEdit = {
+    "username": req.userData.username,
+    "password": body.password
+  }
+  const result = editData('admin', "0", userEdit)
+  if (result) {
+    return res.status(200).send('data saved, will use for next login')
   } else {
-    // called if user is already exists
-    res.status(404).send('User admin not found')
+    // called if request body object key is lacking
+    res.status(400).send('Bad request')
   }
 })
 
--- routes/user/customer/getCustomer.js
+++ routes/user/customer/getCustomer.js
@@ -6,23 +6,17 @@ const shapeObject = require('../../../helpers/shapeObjectHelper')
 const app = express.Router()
 
 app.get('/user', auth.verifyJwt(['admin', "customer"]), (req, res) => {
-  //untuk mengecek key yang dimasukkan didalam query
-  let keys = ['username']
-  const cekKey = shapeObject(req.query, keys)
-  if (!cekKey) {
-    return res.status(400).send('bad request cuy')
-  }
-  //Cek availability of user in database
-  const isUserAvailable = getData('customer', req.query)
-  if (!isUserAvailable.length) { return res.status(404).send('username is not found') }
-
-  const body = req.query
-  if (!body) { return res.status(400).send('please input username') }
-  const result = getData('customer', body)
-  if (result) {
-    res.send(result)
-  } else {
-    res.status(400).send('Bad request')
+  //** get userData from jwt */
+  try {
+    const { id, username, password } = req.userData
+    const result = {
+      id,
+      username,
+      password
+    }
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(500).send(error)
   }
 })
 
