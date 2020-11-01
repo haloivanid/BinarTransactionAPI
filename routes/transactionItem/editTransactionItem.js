ffe295902be11fbacff749a7bd57ffd9ef1af7f7
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-11-02 06:59:01 +0700

    resolve problem in edit transaction item

--- routes/transactionItem/editTransactionItem.js
+++ routes/transactionItem/editTransactionItem.js
@@ -5,13 +5,20 @@ const app = express.Router()
 
 app.patch('/transaction/item', auth.verifyJwt(['admin', "customer"]), (req, res) => {
     if (Object.keys(req.body).length == 0) return res.status(400).send('body not allowed')
+    //** mengecek apakah user memasukkan string kosong ke dalam body */
+    for (const keys in req.body) {
+        if (req.body[keys] == "") return res.status(400).send("value of keys in body can't be empty")
+    }
     if (!req.query.id) return res.status(400).send('id in query needed')
     if (!req.body.itemId) {
         return res.status(400).send('item id in body needed')
     }
     else {
-        const { transactionId, itemId, quantity } = req.body
+        const { userId, transactionId, itemId, quantity } = req.body
+        const isUserExits = getData('customer', { id: userId })
+        if (!isUserExits.length) return res.status(404).send("user not found")
         const transactionItem = {
+            userId,
             transactionId,
             itemId,
             quantity,
